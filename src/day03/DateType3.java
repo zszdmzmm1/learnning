ce44cd7d8f721bf86308aee2b6e2f462e19bed8c
zszdmzmm <1075534380@qq.com>
2023-05-26 14:54:19 +0800

    变量类型强制转换

---
+++ src/day03/DateType3.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class DateType3 {
+    public static void main(String[] args) {
+        double dou1 = 19.9;
+        int int1 = (int)dou1;
+
+        long long1 = 129;
+        short sh1 = (short) long1;
+        byte by1 = (byte) long1;
+
+
+        System.out.println(int1);
+        System.out.println(sh1);
+        System.out.println(by1);
+    }
+}
