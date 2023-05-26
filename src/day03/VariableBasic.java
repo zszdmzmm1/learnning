3b8ace9cfeda020641fedd35c4f87a9f5a65c950
zszdmzmm <1075534380@qq.com>
2023-05-26 11:07:02 +0800

    变量

---
+++ src/day03/VariableBasic.java
@@ -0,0 +1,21 @@
+package day03;
+
+public class VariableBasic {
+    public static void main(String[] args) {
+        // 数据类型 变量名 = 值；
+        int count = 2;
+
+        //变量的使用
+        System.out.println(count);
+
+        //变量的声明
+        int size;
+
+        //System.out.println(size); S
+        //赋值
+        size = 1001;
+        System.out.println(size);
+
+        //int size = 6; 不可复用
+    }
+}
