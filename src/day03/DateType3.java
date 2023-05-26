5ec7768f38662696b2adf57c689c89cf95428e10
zszdmzmm <1075534380@qq.com>
2023-05-26 20:34:24 +0800

    数据类型

--- src/day03/DateType3.java
+++ src/day03/DateType3.java
@@ -1,17 +1,28 @@
 package day03;
 
+/**
+ * 数据类型的强制转换与转换损耗
+ */
+
 public class DateType3 {
     public static void main(String[] args) {
         double dou1 = 19.9;
+        //取整损耗
         int int1 = (int)dou1;
 
-        //损耗
-        long long1 = 129;
-        //无损
-        short sh1 = (short) long1;
-        //溢出
-        byte by1 = (byte) long1;
 
+        long long1 = 128L;
+        //无损
+        short sh1 = (short)long1;
+        //溢出
+        byte by1 = (byte)long1;
+
+        /* 溢出原理：
+            0 00000000 ... 1111111(127) + 0 0000000 ... 00000001(1) = 0 0000000 ... 100000000(128) 其中首位为符号位
+            强制转化为byte，截住后8位，1 0000000。同理，第一位为符号位，即负数。
+            后7位求原码，取反加一：0000000 -> 1111111 -> 10000000(128)
+            故结果为-128。
+        */
 
         System.out.println(int1);
         System.out.println(sh1);
