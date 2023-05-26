ba7402fefc08617b9d1ac4513acc34fb945c5b3f
zszdmzmm <1075534380@qq.com>
2023-05-26 19:11:01 +0800

    数据类型

--- src/day03/DateType.java
+++ src/day03/DateType.java
@@ -1,16 +1,19 @@
 package day03;
 
+/**
+ * 简单介绍基本数据类型
+ */
 public class DateType {
     public static void main(String[] args) {
         //整形
-        byte a = 6;
-        short b = 6;
-        int c = 6;
-        long d = 6L;
+        byte a = 6;     //8bit, 1字节；
+        short b = 6;    //16bit，2字节
+        int c = 6;      //32bit，4字节
+        long d = 6L;    //64bit， 8字节
 
         //浮点型
-        float e = 3.5F;
-        double f = 3.234;
+        float e = 3.5F;     //单精度 32位
+        double f = 3.234;   //双精度 64位
 
 
         //字符型
