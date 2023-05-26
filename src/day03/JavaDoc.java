a08ac01abdfd43423a97fca0112477ac3315a2dd
zszdmzmm <1075534380@qq.com>
2023-05-26 09:59:21 +0800

    第一次提交

---
+++ src/day03/JavaDoc.java
@@ -0,0 +1,15 @@
+package day03;
+public class JavaDoc {
+    /**
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //  kk
+        /*fjas
+
+        */
+        System.out.println("!!!!!");
+    }
+}
