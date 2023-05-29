9422a2beebbc62ebe7a3729f07d892e6072a9daf
zszdmzmm <1075534380@qq.com>
2023-05-29 14:22:39 +0800

    运算符

---
+++ src/day0529/Arithmetic2.java
@@ -0,0 +1,18 @@
+package day0529;
+
+import java.util.Scanner;
+
+public class Arithmetic2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int hundreds = num / 100;
+        int tens = num / 10 % 10;
+        int units = num % 10;
+
+        System.out.println("百位数:" + hundreds);
+        System.out.println("十位数:" + tens);
+        System.out.println("个位数:" + units);
+    }
+}
