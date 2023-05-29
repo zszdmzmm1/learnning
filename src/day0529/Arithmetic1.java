c05be7515c5cfb3d1d3054aba8616e7209a4a6d5
zszdmzmm <1075534380@qq.com>
2023-05-29 11:59:18 +0800

    运算符

---
+++ src/day0529/Arithmetic1.java
@@ -0,0 +1,25 @@
+package day0529;
+
+public class Arithmetic1 {
+    public static void main(String[] args) {
+        int i1 = 18;
+        int i2 = 28;
+        int i =  i1++;
+
+        System.out.print("i = " + i ); //18
+        System.out.println("i1 = " + i1); //19
+
+        i = ++i1;
+        System.out.print("i = " + i ); //20
+        System.out.println("i1 = " + i); //20
+
+        i = i2--;
+        System.out.print("i = " + i ); //28
+        System.out.println("i2 = " + i2); //27
+
+        i = i2++;
+        System.out.print("i = " + i ); //27
+        System.out.println("i2 = " + i2); //28
+    }
+}
+
