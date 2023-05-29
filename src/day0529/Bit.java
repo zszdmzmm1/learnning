bbe7b06bfc12d3ed18795f51c730cfb6306688b8
zszdmzmm <1075534380@qq.com>
2023-05-29 17:15:44 +0800

    运算符

---
+++ src/day0529/Bit.java
@@ -0,0 +1,19 @@
+package day0529;
+public class Bit {
+    public static void main(String[] args) {
+        byte num = 1;
+        System.out.println("num = " + (num << 14));
+
+        int a = 12;
+        int b = 5;
+
+        System.out.println(a & b);
+        System.out.println(a ^ b);
+
+
+        System.out.println("a = " + (a ^ b ^ b));
+        System.out.println("b = " + (a ^ b ^ a));
+
+
+    }
+}
