81f464bbb4dfff641b1a4604b7c2422182630206
zszdmzmm <1075534380@qq.com>
2023-06-06 16:04:27 +0800

    更新

--- src/day0606/BubbleSort.java
+++ src/day0606/BubbleSort.java
@@ -1,19 +1,22 @@
 package day0606;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arrs = new int[]{-2, 45, 0, 11, -9};
+        int[] arrs = new int[]{-2, 45, 0, 11, -9, 23, 11, -11};
         for (int arr : arrs) {
             System.out.print(arr + " ");
         }
         System.out.println();
 
-        int temp = 0;
+        int temp;
         for (int i = 0; i < arrs.length - 1; i++) {
             for (int j = 0; j < arrs.length - i - 1; j++) {
-                if (arrs[j] >= arrs[j + 1]) {
+                if (arrs[j] > arrs[j + 1]) {
                     temp = arrs[j];
                     arrs[j] = arrs[j + 1];
+
                     arrs[j + 1] = temp;
                 }
             }
