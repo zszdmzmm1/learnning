6a3319d95f7d858dcc47e1d724556c4d56f72f63
zszdmzmm <1075534380@qq.com>
2023-06-07 23:06:58 +0800

    更新

--- src/day0607/md/readme.md
+++ src/day0607/md/readme.md
@@ -1,1 +1,5 @@
+![img_1.png](img_1.png)
+
+以上的代码存储过程是：
+
 ![img.png](img.png)
--- src/day0607/state/StudentTest.java
+++ src/day0607/state/StudentTest.java
@@ -6,25 +6,17 @@ public class StudentTest {
         st1.ID = 191201;
         st1.age = 19;
         st1.name = "LSH";
-
         Dog dog1 = new Dog();
         dog1.ID = 1;
         dog1.color = "black";
         dog1.name = "ff";
-
         Dog dog2 = new Dog();
         dog2.ID = 2;
         dog2.color = "white";
         dog2.name = "gg";
-
-
         Dog[] ds1 = new Dog[]{dog1, dog2};
-
         st1.dogs = new Dog[]{dog1};
         //st1.dogs = ds1;
-
-
-
         //System.out.println("学生(" + st1.ID + "学号)的信息：学号=" + st1.ID + "、姓名=" + st1.name + "、年龄=" + st1.age);
         st1.infoShow();
 
