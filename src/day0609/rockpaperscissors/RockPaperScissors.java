f1bad1dc80155ef44c726fb58b82ee07402371ca
zszdmzmm <1075534380@qq.com>
2023-06-09 17:13:20 +0800

    石头剪刀布

--- src/day0609/rockpaperscissors/RockPaperScissors.java
+++ src/day0609/rockpaperscissors/RockPaperScissors.java
@@ -6,8 +6,6 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-
         System.out.println("Enter rock, paper, scissors:");
         Player.show(sc.next());
         ComputerChoice.show(ComputerChoice.getChoice((int)(Math.random() * 3 + 1)));
