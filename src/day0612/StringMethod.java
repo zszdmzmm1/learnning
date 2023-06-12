3d008ce0ab7c2627a3416dc3c226202f96bf84f9
zszdmzmm <1075534380@qq.com>
2023-06-13 00:32:47 +0800

    String

--- src/day0612/StringMethod.java
+++ src/day0612/StringMethod.java
@@ -1,18 +1,78 @@
 package day0612;
 
-import com.google.gson.Gson;
+import java.util.Locale;
+import java.util.Objects;
 
 public class StringMethod {
     public static void main(String[] args) {
-        StringBuffer sMessage = new StringBuffer("jka");
+        /*StringBuffer sMessage = new StringBuffer("jka");
         StringBuffer sFinalMessage =  sMessage.append("_").append("yeah");
+        System.out.println("sFinalMessage = " + sFinalMessage);
         String sFinal = sFinalMessage.toString();
         System.out.println("sResult = " + sFinal);
 
         String s = new String();
-        System.out.println("s = " + s);
+        System.out.println("s = " + s);*/
 
 
+
+        String forTest = new String("There is a girl in my sight.");
+
+        /*将String修改为char数组。*/
+        System.out.println(forTest.toCharArray());
+        System.out.println(forTest.toCharArray()[0]);
+
+
+        /*比较两个String类型是否相同，输出类型是boolean值，若用 == 来比较，比较的是地址*/
+        forTest.equals("jisjfi");
+        //也可写作：
+        Objects.equals("forTest", "jisjfi");//需要导入 import java.util.Objects;
+
+
+        /*变为大写，同理，toLowerCase*/
+        System.out.println(forTest.toUpperCase(Locale.ROOT));
+        System.out.println(forTest.toLowerCase(Locale.ROOT));
+
+        /*输入index，输出对应位置的char值 */
+        System.out.println(forTest.charAt(3));
+
+        /*同上，但输出unicode值 */
+        System.out.println(forTest.codePointAt(4));
+
+
+        /**/
+        System.out.println(forTest.codePointCount(0, 10));
+
+        /*对象与参数String在字典序的相对位置，在前为负数，相等为0，在后为正数*/
+        System.out.println(forTest.compareTo("There is a girl in my sight."));
+        System.out.println(forTest.compareTo("Z"));
+        System.out.println(forTest.compareTo("A"));
+
+
+        /*链接*/
+        System.out.println(forTest.concat("!!!!!"));
+
+        /*是否包含，大小写敏感*/
+        System.out.println(forTest.contains("there"));
+        System.out.println(forTest.contains("There"));
+
+        /*forTest.contentEquals(StringBuffer sb);*/
+
+
+        /*插入，背不下来，有用再查*/
+        char[] ch = {'s','f'};
+        forTest.getChars(2,3,ch,0);
+        System.out.println(ch[0]);
+
+        /*返回第一次出现的索引，无则返回-1，同理还有lastIndexOf*/
+        System.out.println(forTest.indexOf('e'));
+        System.out.println(forTest.indexOf('-'));
+        System.out.println(forTest.lastIndexOf('e'));
+        System.out.println(forTest.lastIndexOf('-'));
+
+        /*不解释*/
+        forTest.length();
+        System.out.println(forTest.repeat(3));
 
     }
 
