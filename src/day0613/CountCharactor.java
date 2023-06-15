8287dd4adf7feac01ede9f2a1490e790a7721dec
zszdmzmm <1075534380@qq.com>
2023-06-15 23:23:50 +0800

    集合

--- src/day0613/CountCharactor.java
+++ src/day0613/CountCharactor.java
@@ -3,32 +3,27 @@ package day0613;
 
 import java.util.*;
 
+/**
+ * 输入一串String，输出每个字母数字有几个
+ */
 
-public class MyList {
+
+public class CountCharactor {
     public static void main(String[] args) {
+        System.out.print("请输入一串字符：");
+        /*拆分并最终放入List中*/
         Scanner sc = new Scanner(System.in);
-        List<Character> letterList = new ArrayList<>();
-        letterList.add('a');
-        letterList.add('r');
-        letterList.add('g');
-        letterList.add('n');
-        letterList.add('a');
-        letterList.add('2');
-        letterList.add('v');
-        letterList.add('r');
-        letterList.add('g');
-        letterList.add('5');
-        letterList.add('6');
-        letterList.add('7');
-        letterList.add('g');
-        letterList.add('4');
-        letterList.add('n');
-        letterList.add('s');
-        letterList.add('t');
-        System.out.println(letterList);
+        char[] letter = sc.next().toCharArray();
+        Character[] cLetter = new Character[letter.length];
+        for (int i = 0; i < cLetter.length; i++) {
+            cLetter[i] = letter[i];
+        }
+        List<Character> letterList = Arrays.asList(cLetter);
+
 
 
         Map<Character, Integer> map = new HashMap<>();
+        /*遍历List在Map中添加，更新*/
         for (Character character : letterList) {
             if (!map.containsKey(character)) {
                 map.put(character, 1);
@@ -39,7 +34,6 @@ public class MyList {
             map.put(character, num);
         }
         Set<Map.Entry<Character, Integer>> charSet = map.entrySet();
-        System.out.println(charSet);
 
         for (Map.Entry<Character, Integer> h : charSet) {
             System.out.println(h.getKey() + ":" + h.getValue());
