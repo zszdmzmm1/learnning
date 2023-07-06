481687d8f606c75f9c3658de016d9d894cce368a
zszdmzmm <1075534380@qq.com>
2023-07-06 11:59:43 +0800

    URL 的 inputStream,URLconnect。

--- src/test/java/day0706/URLReadingTest.java
+++ src/test/java/day0706/URLReadingTest.java
@@ -1,17 +1,22 @@
 package day0706;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class URLReadingTest {
     @Test
     void readingDirectlyFromAURL() throws IOException {
-        URL oracle = new URL("https://www.baidu.com/");
+        URL oracle = new URL("https://www.oracle.com/");
 /*        InputStream inputStream = oracle.openStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader in = new BufferedReader(inputStreamReader);*/
@@ -25,5 +30,24 @@ public class URLReadingTest {
 
         in.close();
 
+    }
+
+    @Test
+    @DisplayName("测试连接时间超时。")
+    void readingDirectlyFromAURL1() throws IOException {
+        URL oracle = new URL("https://www.oracle.com/");
+
+        URLConnection uc = oracle.openConnection();
+        uc.setConnectTimeout(1);
+        uc.setReadTimeout(2);
+
+        Assertions.assertThrows(SocketTimeoutException.class, () -> {
+            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            String inputLine;
+            while((inputLine = in.readLine()) != null){
+                System.out.println(inputLine);
+            }
+            in.close();
+        });
     }
 }
