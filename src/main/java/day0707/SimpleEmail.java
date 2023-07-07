5b3ce2c49286e604c7b2c8510b7e4316d9cffc31
zszdmzmm <1075534380@qq.com>
2023-07-07 18:20:24 +0800

    代码实现自动发送消息

---
+++ src/main/java/day0707/SimpleEmail.java
@@ -0,0 +1,48 @@
+package day0707;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+
+import java.util.Properties;
+
+public class SimpleEmail {
+    public static void main(String [] args) throws Exception {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.qq.com");
+        prop.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("***@qq.com", "password");
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        // who you are
+        message.setFrom(new InternetAddress("from@qq.com"));
+        // send to ...
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("to@qq.com"));
+
+        message.setSubject("这是一个邮件。");
+
+        String msg = "哇哈哈<a>https://baidu.com</a>";
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+
+        System.out.println("Sent message successfully....");
+    }
+}
