cf8d3ef87f20c762d572f6213f62673c82f7815a
zszdmzmm <1075534380@qq.com>
2023-07-08 21:17:14 +0800

    爬小鱼网二手信息，Elements的相关操作对象化

---
+++ src/main/java/day0707/fetchingsecondhandgoods/ElementsDealing.java
@@ -0,0 +1,72 @@
+package day0707.fetchingsecondhandgoods;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.jsoup.select.Elements;
+
+import java.util.List;
+import java.util.Properties;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ElementsDealing {
+    private String qqEmail;
+    private String password; //授权码
+    private String toEmail;
+
+
+    public String getElementsString(List<Elements> itemlist){
+        String msg = "";
+        for (Elements s : itemlist) {
+            msg = msg + "<br>" + s.html() + "</br>" + "<br>" + s.attr("abs:href") + "</br>";
+        }
+        return msg;
+    }
+
+    public void sentMessage(List<Elements> itemlist) throws Exception {
+        String msg = getElementsString(itemlist);
+        getEmail("已发现：", msg);
+    }
+
+    public void getEmail(String title, String msg) throws Exception {
+        System.out.println("正在为您发送消息......");
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.qq.com");
+        prop.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(qqEmail, password);
+            }
+        });
+        Message message = new MimeMessage(session);
+        // who you are
+        message.setFrom(new InternetAddress(qqEmail));
+        // send to ...
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+
+        message.setSubject(title);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+
+        System.out.println("信息发送成功......");
+    }
+}
--- src/main/java/day0707/fetchingsecondhandgoods/Fetching.java
+++ src/main/java/day0707/fetchingsecondhandgoods/Fetching.java
@@ -1,10 +1,5 @@
 package day0707.fetchingsecondhandgoods;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeBodyPart;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMultipart;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,93 +9,71 @@ import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
+
+/**
+ * 在厦门小鱼网爬取二手信息，挑选需要的发送至邮箱
+ */
 public class Fetching {
     public static void main(String[] args) throws Exception {
+        //将需要处理后发送的html存在这里
         List<Elements> itemList = new ArrayList<>();
         int itemC = 0;
-        Boolean isFirst = true;
+        int sentItem = 0;
+        //判断是否覆写最新时间
+        boolean isFirst = true;
         A:
-        for(int i = 1 ;; i++){
+        for (int i = 1; ; i++) {
             Document doc = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55-search-all-orderway-postdate-asc-DESC-page-" + i + ".html").get();
-            Elements elements = doc.select("tr.tr3:has(a[title=开放主题])");
 
+            Elements elements = doc.select("tr.tr3:has(a[title=开放主题])");
             File file = new File("src/main/java/day0707/fetchingsecondhandgoods/time.txt");
+            //首次使用写入时间信息
             byte[] bytes = new byte[1024];
-            if(!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
-                OutputStream outputStream =new FileOutputStream(file);
+                OutputStream outputStream = new FileOutputStream(file);
                 String sLocalDate = LocalDate.now().toString();
                 bytes = sLocalDate.getBytes();
                 outputStream.write(bytes);
                 outputStream.close();
             }
+            //爬取数据前获取上一次爬取的日期
             InputStream inputStream = new FileInputStream(file);
             inputStream.read(bytes);
             inputStream.close();
             String s = new String(bytes);
-            for(Element e : elements){
+            //进一步提取有用信息
+            for (Element e : elements) {
                 Elements item = e.select("a[class=subject_t f14]");
                 Elements date = e.select("p");
-                if(isFirst){
+                //Elements date = e.select("p:not(:has(a))");
+                //首次提取写入最新时间
+                if (isFirst) {
                     OutputStream outputStream = new FileOutputStream(file);
                     outputStream.write(date.html().split("<")[0].getBytes());
+                    //outputStream.write(date.html().getBytes());
+
                     outputStream.close();
                     isFirst = false;
                 }
-                if(date.html().compareTo(s) >= 0){
-                    System.out.println(item.html());
-                    if(item.html().contains("转让")){
+                //记录需要的物品
+                if (date.html().compareTo(s) >= 0) {
+                    //System.out.println(item.html());
+                    if (item.html().contains("转让")) {
                         itemList.add(item);
+                        sentItem++;
                     }
                     itemC++;
-                }else{
-                    System.out.println(itemC);
+                } else {
+                    System.out.println("共发现" + itemC + "个新物品。");
+                    System.out.println("已为您找出" + sentItem + "个匹配物品。");
                     break A;
                 }
-
             }
         }
-        Fetching.rebounce(itemList);
-    }
-
-    public static void rebounce(List<Elements> item) throws Exception {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.qq.com");
-        prop.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("**@qq.com", "ecwqgoayrhircagd");
-            }
-        });
-
-        Message message = new MimeMessage(session);
-        // who you are
-        message.setFrom(new InternetAddress("from@qq.com"));
-        // send to ...
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("to@qq.com"));
-        int size = item.size();
-        message.setSubject("已找到" + size + "个匹配物品");
-        String msg = "";
-        for(Elements s: item) {
-            msg = msg + "<br>" + s.html() + "</br>" + "<br>" + s.attr("abs:href") + "</br>";
-        }
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
-
-        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
-
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
-
-        message.setContent(multipart);
-
-        Transport.send(message);
-
-        System.out.println("Sent message successfully....");
+        //发送消息
+        ElementsDealing elementsDealing = new ElementsDealing("*****", "*******", "********");
+        elementsDealing.sentMessage(itemList);
     }
 }
