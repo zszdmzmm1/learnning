7abb6cef407f12b9fb08f53f72e59130abc023b8
zszdmzmm <1075534380@qq.com>
2023-07-10 11:25:45 +0800

    修改逻辑错误，添加环境变量

--- src/main/java/day0707/fetchingsecondhandgoods/Fetching.java
+++ src/main/java/day0707/fetchingsecondhandgoods/Fetching.java
@@ -20,46 +20,49 @@ public class Fetching {
         List<Elements> itemList = new ArrayList<>();
         int itemC = 0;
         int sentItem = 0;
+        String s= null;
+        File file = new File("src/main/java/day0707/fetchingsecondhandgoods/time.txt");
+        //首次使用写入时间信息
+        byte[] bytes = new byte[10];
+        if (!file.exists()) {
+            file.createNewFile();
+            OutputStream outputStream = new FileOutputStream(file);
+            String sLocalDate = LocalDate.now().toString();
+            bytes = sLocalDate.getBytes();
+            outputStream.write(bytes);
+            outputStream.close();
+        }
         //判断是否覆写最新时间
         boolean isFirst = true;
         A:
-        for (int i = 1; ; i++) {
+        for (int i = 1; i < 100; i++) {
             Document doc = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55-search-all-orderway-postdate-asc-DESC-page-" + i + ".html").get();
 
-            Elements elements = doc.select("tr.tr3:has(a[title=开放主题])");
-            File file = new File("src/main/java/day0707/fetchingsecondhandgoods/time.txt");
-            //首次使用写入时间信息
-            byte[] bytes = new byte[1024];
-            if (!file.exists()) {
-                file.createNewFile();
-                OutputStream outputStream = new FileOutputStream(file);
-                String sLocalDate = LocalDate.now().toString();
-                bytes = sLocalDate.getBytes();
-                outputStream.write(bytes);
-                outputStream.close();
-            }
-            //爬取数据前获取上一次爬取的日期
-            InputStream inputStream = new FileInputStream(file);
-            inputStream.read(bytes);
-            inputStream.close();
-            String s = new String(bytes);
+            Elements elements = doc.select("tr.tr3:has(a[title=开放主题]), tr.tr3:has(a[title=热门主题])");
+
             //进一步提取有用信息
             for (Element e : elements) {
                 Elements item = e.select("a[class=subject_t f14]");
-                Elements date = e.select("p");
-                //Elements date = e.select("p:not(:has(a))");
+                //Elements date = e.select("p");
+                Elements date = e.select("p:not(:has(a))");
                 //首次提取写入最新时间
                 if (isFirst) {
-                    OutputStream outputStream = new FileOutputStream(file);
-                    outputStream.write(date.html().split("<")[0].getBytes());
-                    //outputStream.write(date.html().getBytes());
+                    //爬取数据前获取上一次爬取的日期
+                    InputStream inputStream = new FileInputStream(file);
+                    inputStream.read(bytes);
+                    inputStream.close();
+                    s = new String(bytes);
 
+                    //写入最新时间
+                    OutputStream outputStream = new FileOutputStream(file);
+                    //outputStream.write(date.html().split("<")[0].getBytes());
+                    outputStream.write(date.html().getBytes());
                     outputStream.close();
                     isFirst = false;
                 }
                 //记录需要的物品
                 if (date.html().compareTo(s) >= 0) {
-                    //System.out.println(item.html());
+                    System.out.println(item.html());
                     if (item.html().contains("转让")) {
                         itemList.add(item);
                         sentItem++;
@@ -74,7 +77,7 @@ public class Fetching {
         }
         //发送消息
         if (itemList.size() != 0) {
-            ElementsDealing elementsDealing = new ElementsDealing("*****", "*******", "********");
+            ElementsDealing elementsDealing = new ElementsDealing(System.getenv("qqEmail"), System.getenv("password"), System.getenv("toEmail"));
             elementsDealing.sentMessage(itemList);
         }
     }
